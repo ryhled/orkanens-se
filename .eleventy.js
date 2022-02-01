058686473f75ca3cbc540d7e1d3e12310718f56e
Daniel Ryhle <daniel@ryhle.se>
2022-02-01 17:29:01 +0100

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ const pluginSass = require("eleventy-plugin-sass");
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPlugin(pluginSass, {});
+    eleventyConfig.addPassthroughCopy("CNAME");
     return {
     }
   };
