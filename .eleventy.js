e064ff9cc9e6d4d6aa6333d7dfe5c0ed4a7708bc
Daniel Ryhle <daniel@ryhle.se>
2022-02-01 18:18:41 +0100

    formatting & navbar

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,8 @@
 const pluginSass = require("eleventy-plugin-sass");
 
-module.exports = function(eleventyConfig) {
-    eleventyConfig.addPlugin(pluginSass, {});
-    eleventyConfig.addPassthroughCopy("CNAME");
-    return {
-    }
-  };
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(pluginSass, {});
+  eleventyConfig.addPassthroughCopy("CNAME");
+  return {
+  }
+};
